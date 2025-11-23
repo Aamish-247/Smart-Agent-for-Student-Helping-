638ea32535dceccbb67ff00a5e71efad9fc35a77
Aamish-247 <arainzada247@gmail.com>
2025-11-23 23:44:30 +0500

    Code Updated

--- basic.py
+++ basic.py
@@ -1,3 +1,10 @@
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 from langchain_core.messages import SystemMessage
+load_dotenv()
+import os
+
+api_key = os.getenv("Groq_API_KEY")
+
+model = ChatGroq(model="openai/gpt-oss-20b", api_key= api_key)
+
